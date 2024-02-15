fbab3dee5bf17be3900f7635e62b064e44babda1
rastylus <ryan.p.riggs@gmail.com>
2024-02-15 08:00:12 -0600

    components

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Favorite Photos';
+  image1 = 'https://rvb-img.reverb.com/image/upload/s--MINK5bci--/a_0/f_auto,t_large/v1707004903/en3kgrjpoeit2fgqnjyj.jpg';
+  image2 = 'https://rvb-img.reverb.com/image/upload/s--npGZ-X1K--/a_0/t_card-square/v1707167944/szhw0kfozepfghvkpbll.jpg';
+  image3 = 'https://image.mux.com/AliviPHFYvRhi6Sq4w3nNySxsE00wYFVhfa8uDiMhGLA/thumbnail.jpg?token=eyJhbGciOiJSUzI1NiIsImtpZCI6Ik5CY3o3Sk5RcUNmdDdWcmo5MWhra2lEY3Vyc2xtRGNmSU1oSFUzallZMDI0IiwidHlwIjoiSldUIn0.eyJzdWIiOiJBbGl2aVBIRll2UmhpNlNxNHczbk55U3hzRTAwd1lGVmhmYTh1RGlNaEdMQSIsImV4cCI6MTcxMDA2MDAyOCwiYXVkIjoidCIsInRpbWUiOjB9.n2rsEHv-qgToDvG7kWb3OV-qgVQSefLgLzjNdMAhwxGoIYD7zIHpqUtNiZO860Z8sCcA_r2vbP9Aq3o4ri_LzcCSZv8xSv8nmDrBjh-A4l6aZZ6s3UyAcC3NS_Rzb72ZvLxAyjy1ZEfUfFooQf7HpEpK4Pe3U4wFTCRNU5aY76IesyUd4vQGyU_JbHB56n1MC_mVKYmS-nHYXXZxx9CcyKQTIdhK_utflkRj5YqQ5brzkO245Jgjm8Q5hTH5R8c45vQ8Dj3C4C_DBiJBBvE62b98F_2hJTtlCBzCqjjvLxJYpy_2lSELX2awbSkyUgsCh4OdgP4gr0J5c7cM9Zw1mg';
 
   constructor() { }
 
